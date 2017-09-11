9a19f526bf3d387b422e0789f859a72376a2d421
Itzel Jimenez <itzeljimenez.do@gmail.com>
2017-09-11 18:39:36 -0500

    compomemte dinamico

--- app.js
+++ app.js
@@ -1,42 +1,33 @@
-Vue.component('task-list', {
+Vue.component('message', {
+
+	props: ['title', 'body', 'image'],
 
 	template: `
 
-		<div>
+		        <v-layout>
+		          <v-flex xs12 sm6 offset-sm3>
+		            <v-card>
+		              <v-card-media src="https://i1.wp.com/medioambienteynaturaleza.com/wp-content/uploads/2015/08/Animales-tiernos-aj.jpg" height="500px">
+		              </v-card-media>
+		              <v-card-title primary-title>
+		                <div>
+		                  <h3 class="headline mb-0">{{ title }}</h3>
+		                  <div>{{ body }}</div>
+		                </div>
+		              </v-card-title>
+		              <v-card-actions>
+		                <v-btn flat class="orange--text">Share</v-btn>
+		                <v-btn flat class="orange--text">Explore</v-btn>
+		              </v-card-actions>
+		            </v-card>
+		          </v-flex>
+		        </v-layout>
 
-			<task v-for="task in tasks">{{ task.task }}</task>
-
-		</div>'
-
-	`,
-
-	data(){
-
-		return {
-
-			tasks: [
-
-				{ task: 'Go to the store', complete: true },
-
-				{ task: 'Go to the email', complete: true },
-
-				{ task: 'Go to the farm', complete: true },
-
-				{ task: 'Go to work', complete: true }, 
-
-			]
-
-		}
-
-	}
+	`
+	
 
 })
 
-Vue.component('task', {
-
-	template: '<li><slot></slot></li>'
-
-})
 new Vue({
 
 	el: '#root'
