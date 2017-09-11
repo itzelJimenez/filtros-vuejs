03fa3dbd4d9ec6975bf7cd09109856513e0b5a73
Itzel Jimenez <itzeljimenez.do@gmail.com>
2017-09-11 17:37:08 -0500

    adición de lista dinámica

---
+++ app.js
@@ -0,0 +1,44 @@
+Vue.component('task-list', {
+
+	template: `
+
+		<div>
+
+			<task v-for="task in tasks">{{ task.task }}</task>
+
+		</div>'
+
+	`,
+
+	data(){
+
+		return {
+
+			tasks: [
+
+				{ task: 'Go to the store', complete: true },
+
+				{ task: 'Go to the email', complete: true },
+
+				{ task: 'Go to the farm', complete: true },
+
+				{ task: 'Go to work', complete: true }, 
+
+			]
+
+		}
+
+	}
+
+})
+
+Vue.component('task', {
+
+	template: '<li><slot></slot></li>'
+
+})
+new Vue({
+
+	el: '#root'
+	
+})
